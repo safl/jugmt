4973701d6778e3218dd72ee946133a57c1ed5cf9
Simon A. F. Lund <os@safl.dk>
2024-07-19 23:21:16 +0200

    feat(tests): add test of snake/pascal conversion
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ tests/test_case_conversion.py
@@ -0,0 +1,13 @@
+from jugmt.document import pascal_to_snake, snake_to_pascal
+
+PASCAL = [
+    "Foo",
+    "FooBar",
+    "SomeVeryLongName",
+]
+
+
+def test_conversions():
+
+    for pascal in PASCAL:
+        assert snake_to_pascal(pascal_to_snake(pascal)) == pascal
